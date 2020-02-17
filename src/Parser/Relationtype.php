4ab38692e3b75d584b0a336ff2a4fe075f9415cd
Mehdi Jalilvand <mhd.jalilvand@gmail.com>
2020-02-17 17:34:18 +0330

    comments in relation type class

--- src/Parser/Relationtype.php
+++ src/Parser/Relationtype.php
@@ -7,12 +7,25 @@ namespace DbmlParser\Parser;
  */
 class Relationtype
 {
+  /**
+   * relation type
+   * @var int ONE2MANY 
+   * @var int MANY2ONE 
+   * @var int ONE2ONE 
+   */
   public const ONE2MANY = 1;
   public const MANY2ONE = 2;
   public const ONE2ONE = 3;
   
-  public $type;
-  public function __construct(string $type){
+  protected $type;
+  
+  /**
+   * column constructor.
+   * @param string $type
+   * @throws Exception
+   */
+  public function __construct(string $type) 
+  {
     switch($type ){
       case '<': $this->type =  self::ONE2MANY;break;
       case '>': $this->type =  self::MANY2ONE;break;
@@ -20,7 +33,13 @@ class Relationtype
     }
   }
   
-  public function __toString(){
+  /**
+   * __toString magic function
+   * @return string $type <->
+   * @throws Exception
+   */  
+  public function __toString():string
+  {
     switch($this->type ){
       case self::ONE2MANY: return  '<';break;
       case self::MANY2ONE: return  '>';break;
