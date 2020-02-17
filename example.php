b9355ba6e6112abb88164d8b50280b9e4b26b57b
Mehdi Jalilvand <mhd.jalilvand@gmail.com>
2020-02-17 13:12:26 +0330

    update example script

--- example.php
+++ example.php
@@ -3,5 +3,8 @@ require 'vendor/autoload.php';
 use DbmlParser\Parser;
 $parser = new Parser('tests/test.dbml');
 foreach($parser->tables as $table){
-  echo $table->name.':['.implode(',',$table->columns).']'.PHP_EOL;
+  echo $table->name.':'.PHP_EOL;
+  foreach($table->columns as $column){
+    echo "\t".json_encode($column).PHP_EOL;
+  }
 }
